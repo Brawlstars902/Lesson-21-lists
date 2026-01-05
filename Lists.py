c2f1208071c58a7ed6f8abdccb000c7bdc13622f
Brawlstars902 <aparna.rautela@gmail.com>
2026-01-05 10:01:22 +0530

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,12 @@
+empty_list=[]
+print()
+
+numbers=[1,2,3,4]
+print(numbers)
+
+triples=[1,2,3]
+print(triples)
+
+alist=['start',2,4,6,8,10,'end']
+print('The normal list is',alist)
+print('The reversed list is',alist[::-1])
